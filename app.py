fb03d5d746e624219de8b76cfe2e8952fff1a671
Rajat Goyal <404rajat@gmail.com>
2020-10-31 00:24:12 +0530

    changes

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def main():
     #Twitter keys
     consumer_key = os.getenv('TWITTER_CONSUMER_KEY', '')
     consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET', '')
-    print("********************", consumer_key)
 
     #Pocket keys
     # p_consumer_key = os.environ.get('POCKET_CONSUMER_KEY')
